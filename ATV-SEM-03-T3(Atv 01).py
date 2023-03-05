f8cc2c3da7158a3a7015f1f722b35a443e8793a7
Ludrachel <ludmylaraquel0501@gmail.com>
2023-03-05 02:22:06 -0300

    first commit

---
+++ ATV-SEM-03-T3(Atv 01).py
@@ -0,0 +1,10 @@
+PI = 3.141592
+raio = float(input("digite o raio:"))
+circunferencia = 2 * PI * raio
+a_circulo = PI * raio * 2
+a_esfera = 4 * PI * raio ** 2
+vol_esfera = 4 / 3 * PI * raio ** 3
+print("circunferência: ", circunferencia)
+print("Área do círculo: ", a_circulo)
+print("Área da esfera ", a_esfera)
+print("volume da esfera: ", vol_esfera)
---
+++ ATV-SEM-03-T3(Atv 03).py
@@ -0,0 +1,6 @@
+dividendo = int(input("digite o dividendo: "))
+divisor = int(input("digite o divisor: "))
+quociente = dividendo // divisor
+resto = dividendo % divisor
+print(f'{dividendo} dividido por {divisor}')
+print(f'é igual a {quociente} e resto {resto}')
